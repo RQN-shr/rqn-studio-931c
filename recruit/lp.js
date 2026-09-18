05bc4785545e3c54b44dc1b5c604f6b9ffdee7ce
RQN-shr <xxxrocketqueenxx@gmail.com>
2026-09-19 04:07:16 +0900

    recruit: add stylesheet loader

---
+++ recruit/lp.js
@@ -0,0 +1,7 @@
+/* R.QueenNail recruit LP: stylesheet loader v1 */
+(function () {
+  var l = document.createElement("link");
+  l.rel = "stylesheet";
+  l.href = "https://rqn-shr.github.io/rqn-studio-931c/recruit/lp.css?v=1";
+  document.head.appendChild(l);
+})();
